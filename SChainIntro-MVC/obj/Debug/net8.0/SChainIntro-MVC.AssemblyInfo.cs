aea8d4cb50bca2dd941263af273e8d78be2bcd56
Oybek  Muxtaraliyev <muxtaraliyevoybek@gmail.com>
2024-06-08 17:32:26 +0500

    from Work-PC

--- SChainIntro-MVC/obj/Debug/net8.0/SChainIntro-MVC.AssemblyInfo.cs
+++ SChainIntro-MVC/obj/Debug/net8.0/SChainIntro-MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SChainIntro-MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48e8fa26d89fb9b237cd0fe004d1a9312704c987")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f76eba379ef2e72593817e17724a55a03dbddcb")]
 [assembly: System.Reflection.AssemblyProductAttribute("SChainIntro-MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SChainIntro-MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
