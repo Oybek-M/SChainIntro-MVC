7d9efcfcb2f2e60c8f47ecf3a49e0900a9da0425
Oybek Muxtaraliyev <160513698+Oybek-M@users.noreply.github.com>
2024-06-10 12:12:28 +0500

    Update Program.cs

--- SChainIntro-MVC/Program.cs
+++ SChainIntro-MVC/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.EntityFrameworkCore;
+using SChainIntro_MVC.BLL.Interfaces;
+using SChainIntro_MVC.BLL.Services;
 using SChainIntro_MVC.Data;
+using SChainIntro_MVC.Data.Interfaces;
+using SChainIntro_MVC.Data.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,21 +11,54 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 
+
 // Connection String | DataBase with MySQL:
-var connectionString = builder.Configuration.GetConnectionString("LocalDB");
+var connectionString = builder.Configuration.GetConnectionString("LocalDB1");
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
 });
+// ____________________
+
+
+
+// Services
+builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
+builder.Services.AddTransient<IFileService, FileService>();
+
+builder.Services.AddTransient<IAuthService, AuthService>();
+builder.Services.AddTransient<IPartnerService, PartnerService>();
+builder.Services.AddTransient<IPostService, PostService>();
+builder.Services.AddTransient<IServiceService, ServiceService>();
+builder.Services.AddTransient<IStaticsService, StaticsService>();
+builder.Services.AddTransient<IVideoService, VideoService>();
+// ____________________
+
+
+
+// HttpContextAccessor
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+// ____________________
+
+
+
+// Auth
+builder.Services.AddAuthentication()
+    .AddCookie("Admin", config =>
+    {
+        config.LoginPath = "/admin/auth/login";
+    });
+// ____________________
+
 
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -30,12 +67,14 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllerRoute(name: "Admin", pattern: "{area: exists}/{controller=Home}/{action=Index}");
+app.MapControllerRoute(
+    name: "admin",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
